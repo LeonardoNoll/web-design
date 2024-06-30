5f40bd5408de432ef988035713550e07558833c4
Leonardo Noll Cruz <leonoll8@hotmail.com>
2024-06-30 16:23:15 -0300

    embelezamento no index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "\index.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'border-spin': {
+          '100%' : { transform: 'rotate(-360deg)' }
+        }
+      },
+    },
+    animation: { 
+      'border-spin': 'border-spin 2s linear infinite'
+    }
   },
   plugins: [],
 }
